acf4904ab1f3c461a7b72dd0651038d3a207b180
Nguyen Thuy Diem <ngthuydiem@gmail.com>
2015-01-15 23:36:15 +0800

    fix bugs in run-sample.sh & run-crispy.sh

--- run-crispy.sh
+++ run-crispy.sh
@@ -1,18 +1,14 @@
-#!/bin/zsh
-
 #input: $INPUT, $K, $B
-PROFILER=(/usr/bin/time -f "%e %M")
 INPUT=$1
 K=$2
 B=$3
 
 UNIQUE_INPUT=$INPUT"_Clean"
 
-./crispy-cuda/bin/preprocess -i $INPUT
-$PROFILER ./crispy-cuda/bin/kmerDist -i $UNIQUE_INPUT -k $K 
-$PROFILER ./crispy-cuda/bin/genDist -i $UNIQUE_INPUT -b $B
+./bin/preprocess -i $INPUT
+./bin/kmerDist -i $UNIQUE_INPUT -k $K 
+./bin/genDist -i $UNIQUE_INPUT -b $B
 
 NUM_READS=`grep '>' $UNIQUE_INPUT | wc -l`
 NUM_FILES=`ls $UNIQUE_INPUT".ndist"* | wc -l`	
-#$PROFILER ./crispy-cuda/bin/aveclust -i $UNIQUE_INPUT -n $NUM_READS -f $NUM_FILES
-$PROFILER ./crispy-cuda/bin/hcluster -i $UNIQUE_INPUT -n $NUM_READS -f $NUM_FILES
+./bin/aveclust -i $UNIQUE_INPUT -n $NUM_READS -f $NUM_FILES
--- run-sample.sh
+++ run-sample.sh
@@ -1,9 +1,6 @@
-#!/bin/zsh
-
-cd ..
-INPUT="crispy-cuda/data/V2Mice_sample1000"
+INPUT="data/V2Mice_sample1000"
 K=6
 B=20
 
-./crispy-cuda/run-crispy.sh $INPUT $K $B
+./run-crispy.sh $INPUT $K $B
 
